de2ac37aef2471b5162029d3c4fb8165cf9ef8ba
mukimchoudhary <161674191+mukimchoudhary@users.noreply.github.com>
2024-06-30 09:31:51 +0000

    Conversion of data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// alert(3+3)
+// sting
+// boolean
+// null= object is datatype
+// undefined = undefined 
+// symbole  = unique
+// object
---
+++ 01_basics/03 _conversionOperation.js
@@ -0,0 +1,11 @@
+let score = 34
+
+console.log(typeof score);
+console.log(typeof(score)); 
+
+let booleanIsloggedIn = boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "Mukim" => true
