edac30cb0d82656d22d30c54bb4e9a80e312e97b
mukimchoudhary <161674191+mukimchoudhary@users.noreply.github.com>
2024-07-01 06:30:54 +0000

    datatype summery

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,31 @@
+// primitive
+
+// 7 type : String, Number, Boolean, Null, Undefined, Symbol,BigInt
+
+/* Dynamically: Type Checking: Performed at runtime.
+Type Declarations: Variables can be used without declaring their type; 
+the type is inferred at runtime. */
+
+/* Statitically: Type Checking: Performed at compile-time.
+Type Declarations: Variables must be declared with a type 
+before they are used */
+
+
+// Reference (Non primitive)
+
+// array, object, function
+
+//let my array; const heros = ["tipu sultan","raja harishchandra"]
+
+/*let my object {
+    name: "mukim"
+    age: 22
+}/*
+
+const myFunction = function(){
+console.log(hello world);
+
+}
+
+console.log (typeof);
+
