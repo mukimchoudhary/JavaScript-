6fc17a61ee8116e81bff99faf0808e1f26cf56dd
mukimchoudhary <161674191+mukimchoudhary@users.noreply.github.com>
2024-07-02 06:04:32 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number (100)
+//console.log(balance);
+
+//console.log(balance.toString().lenght);
+//console.log(balance.toFixed(2)); // for using easy calculation
+
+const otherNumber = 123.983 // it round of the value
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++(Math)++++++++++++++++++++
+
+console.log(Math); 
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));// use for lower value
+console.log(Math.ceil(4.2)); // use for upper value
+
+console.log(Mtah.random()); // give the vale bt 0 to 1
+console.log((Math.random()*10)+1); // to avoide getting zero we add 1
+console.log(Math.floor(mat.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(math.floor(Mtah.random()*(max-min+1))+min);
+// to getting the rang bt 10 to 20 to avoide 0 we add 1
