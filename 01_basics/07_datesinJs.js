1cb08735e562ae23736100ed003806743c3457ad
mukimchoudhary <161674191+mukimchoudhary@users.noreply.github.com>
2024-07-02 06:39:32 +0000

    Date

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,18 @@
+// Date
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(typeof myDate);
+
+//let myCreatDate = new Date(2023,0,23,5,3)
+//console.log(myCreatDate.toLocaleDateString());
+
+let newDate = new Date ()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+console.log(newDate.getTime());
