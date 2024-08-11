24ed531bcaffd00097aa68ca4b1da9863b298dbf
mukimchoudhary <161674191+mukimchoudhary@users.noreply.github.com>
2024-08-11 11:10:23 +0000

    mk

--- 09_advance_one/call.js
+++ 09_advance_one/call.js
@@ -12,3 +12,7 @@ function createUser(username, email, password){
 
 const chai = new createUser("chai","chai@fb.com", "123")
 console.log(chai);
+// i have to read all the source carefully and revision of  javascript 
+// so all the react and the system work propline
+// so nice al the all the concept of all react work and the system of all the sources and the system
+// to all the alert so this is known as the 
