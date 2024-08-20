51b8cbb6209661aef04fd6fbe62ff230df8b836d
mukimchoudhary <161674191+mukimchoudhary@users.noreply.github.com>
2024-08-20 16:12:41 +0000

    evapnac

---
+++ 08_event.js/eventwithap.js
@@ -0,0 +1,11 @@
+/* Event in javascript 
+
+The changes in the state of an object is known as Event
+Event are fired to notify code of "interesting changes" that may affect code execution
+
+mouse event(click, double click  etc)
+Keyboard events (keypress, keyup, keydown)
+From event (submit etc.)
+Print event & many more
+
+*/
